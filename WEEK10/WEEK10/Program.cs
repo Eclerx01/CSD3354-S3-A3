03de8502a8585d62754b37c7436851b2d3281cd5
aman chahal <amanchahal497@gmail.com>
2019-07-10 08:47:33 -0400

    text files

--- WEEK10/WEEK10/Program.cs
+++ WEEK10/WEEK10/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace WEEK10
     {
         static void Main(string[] args)
         {
+            var tp = new TextProcessing();
+            tp.Run();
+            Console.Read();
+        }
+    class TextProcessing
+        {
+            public void Run()
+            {
+                using (StreamReader file = new StreamReader("C:/Users/Admin/ASSIGNMENT/WEEK10/WEEK10/TextFile1.TXT"))
+                {
+                    int counter = 0;
+                    string ln;
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        Console.WriteLine(ln);
+                        counter++;
+                    }
+                    file.Close();
+                    Console.WriteLine($"File has {counter} lines.");
+                }
+            }
         }
     }
 }
