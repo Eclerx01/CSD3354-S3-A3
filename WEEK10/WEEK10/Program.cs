552543cdcf312fd8ff51ececdfd287046ac89413
aman chahal <amanchahal497@gmail.com>
2019-07-16 23:26:55 -0400

    asig

--- WEEK10/WEEK10/Program.cs
+++ WEEK10/WEEK10/Program.cs
@@ -13,12 +13,16 @@ namespace WEEK10
         {
             var tp = new TextProcessing();
             tp.Run();
-            Console.Read();
+            // new Blue().SayFavoriteFood(Red.FavoriteFood); 
+
+
         }
-    class TextProcessing
+        class TextProcessing
         {
+            Queue<string> names = new Queue<string>();
             public void Run()
             {
+                //open the name files and access data
                 using (StreamReader file = new StreamReader("C:/Users/Admin/ASSIGNMENT/WEEK10/WEEK10/TextFile1.TXT"))
                 {
                     int counter = 0;
@@ -26,11 +30,52 @@ namespace WEEK10
 
                     while ((ln = file.ReadLine()) != null)
                     {
+                        names.Enqueue(ln);
                         Console.WriteLine(ln);
                         counter++;
                     }
                     file.Close();
                     Console.WriteLine($"File has {counter} lines.");
+                    //post condition of while is:
+                    //we now have a data member named names which is a queue!!!!!
+                    Playwithstacks.Run(names);
+                }
+            }
+        }
+        class Playwithstacks
+        {
+            public static void Run(Queue<string> tangerine)
+            {
+                Stack<string> names = new Stack<string>();
+                //how can i access the queue variables "names" here in the class?
+                //iterate over our Queue
+                //get each element
+                //push it into the stack
+                foreach (var item in tangerine)
+                {
+                    names.Push(tangerine.Dequeue());
+                    Console.WriteLine();
+                    Console.WriteLine(item);
+
+                }
+
+                PlayingWithList.Run(names);
+            }
+
+        }
+        class PlayingWithList
+        {
+
+
+            public static void Run(Stack<string> tangerine)
+            {
+                LinkedList<string> names = new LinkedList<string>();
+
+                foreach (var item in tangerine)
+                {
+                    names.AddFirst(item);
+                    Console.WriteLine(item);
+
                 }
             }
         }
